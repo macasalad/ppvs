8020e82e724169a34f25e5668aa60b371d0a917c
macasalad <87970411+macasalad@users.noreply.github.com>
2022-09-10 18:33:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     # title
     col1, col2 = st.columns([1, 6])
     with col1:
-        st.image(progvarlogo, width=90)
+        st.image(progvarlogo, width=110)
     with col2:
         st.image(comeleclogo, width=110)
     st.title("Plurality-Preferential Voting System Program")
